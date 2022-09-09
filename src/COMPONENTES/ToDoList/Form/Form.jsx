5e3f09d54980ec607f8c93061cadd090375dcf5b
BadSh4dow <guidocabrera711@gmail.com>
2022-09-08 22:44:18 -0300

    first commit

---
+++ src/COMPONENTES/ToDoList/Form/Form.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Form = ()=>{
+    return(
+        <form action="javascript:void(0);">
+      <input id="tarea" type="text" name="tarea" placeholder="Descripción de la tarea"/>
+      <select name="prioridad" id="prioridad">
+        <option value="" disabled selected>Prioridad</option>
+        <option value="prioridad-baja">baja</option>
+        <option value="prioridad-media">media</option>
+        <option value="prioridad-alta">alta</option>
+      </select>
+      <button id="agregar">Agregar!</button>
+    </form>
+    )
+}
+export default Form;
---
+++ src/COMPONENTES/ToDoList/ToDoList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Title from "./Title/Title";
+
+const ToDoList =()=>{
+    return(
+        <Title
+            text="Lista de Tareas!"
+        />
+    )
+}
+export default ToDoList;
